23fd38a718233308bbdd740addb16b83c9272d1c
François Sémécurbe <francoissemecurbe@gmail.com>
2024-02-16 10:35:29 +0000

    test

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     var scroller = scrollama();
-    var images = [
-        "carte.svg",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=2",
-        "pieHuile.svg",
-        "https://via.placeholder.com/600x400/FF0000/808080?text=4",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=5",
-        "https://via.placeholder.com/600x400/00FFFF/808080?text=3"
-    ];
 
     function handleStepEnter(response) {
         // Trouver l'image dans le même conteneur 'content' ou 'additional-content' que le step activé
